440bb7f08387a787817c2349d4dac64c7b01f225
shahmatist123 <khggcvfd@gmail.com>
2020-07-13 01:14:43 +0500

    base structure

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+    <v-app id="inspire">
+        <v-navigation-drawer
+                v-model="drawer"
+                app
+        >
+            <v-list dense>
+                <v-list-item link>
+                    <v-list-item-action>
+                        <v-icon>mdi-home</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>Home</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-list-item link>
+                    <v-list-item-action>
+                        <v-icon>mdi-email</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>Contact</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+
+        <v-app-bar
+                app
+                color="indigo"
+                dark
+        >
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+            <v-toolbar-title>Application</v-toolbar-title>
+        </v-app-bar>
+
+        <v-main>
+        </v-main>
+    </v-app>
+</template>
+
+<script>
+
+    export default {
+        name: 'App',
+        props: {
+            source: String,
+        },
+        data: () => ({
+            drawer: null,
+        }),
+    };
+
+</script>
